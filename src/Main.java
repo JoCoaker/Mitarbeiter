e353e940d9fb027059c35c2624b5b7f832bbdef5
Luke <veloject@gmail.com>
2018-04-09 09:46:14 +0200

    Mitarbeiter und Main angefangen

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main
+ */
+public class Main {
+
+    /**
+     * Main Methode um Mitarbeiter anzulegen und auszuprobieren.
+     *
+     * @param args {String[]}
+     */
+    public static void main(String[] args) {
+        Zeitarbeiter zeitarbeiter = new Zeitarbeiter();
+        Angestellter angestellter = new Angestellter();
+        ATAngestellter aTAngestellter = new ATAngestellter();
+    }
+
+}
--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -1,4 +1,52 @@
 
-abstract class Mitarbeiter {
-    
+/**
+ * Mitarbeiter
+ *
+ * @author Lukas Reichert (199034)
+ * @version 1.0.0
+ */
+public abstract class Mitarbeiter {
+
+    // Klassen Variablen
+    protected String vorname;
+    protected String nachname;
+    protected float jahresGehaltBisHeute;
+
+    /**
+     * Konstruktor
+     *
+     * @param vorname {String}
+     * @param nachname {String}
+     */
+    Mitarbeiter(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+
+    /**
+     * Berechnet das Entgelt für den Mitarbeiter.
+     *
+     * @return entgelt {float}
+     */
+    public abstract float entgeltBerechnen();
+
+    @Override
+    public String toString() {
+        return this.vorname + " " + this.nachname;
+    }
+
+    /**
+     * Getter
+     */
+    public String getVorname() {
+        return this.vorname;
+    }
+
+    public String getNachname() {
+        return this.nachname;
+    }
+
+    public float getJahresGehaltBisHeute() {
+        return this.jahresGehaltBisHeute;
+    }
 }
