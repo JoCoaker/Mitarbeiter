401cf385705bfb6352373feebf8af3f2aa1aea2c
felix6u4 <35486339+felix6u4@users.noreply.github.com>
2018-04-12 08:10:39 +0200

    Update ATAngestellter.java

--- src/ATAngestellter.java
+++ src/ATAngestellter.java
@@ -4,12 +4,20 @@
  * @author Felix Ruess (199261)
  * @version 1.0.0
  */
-public class ATAngestellter extends Mitarbeiter {
+public class ATAngestellter implements IMitarbeiter, ISteuerZahler {
 	private float monatsLohn;
+	private float jahresGehaltBisHeute;
+	private String nachname;
+	private String vorname;
+	
 
 	public ATAngestellter(float monatsLohn, String vorname, String nachname) {
-		super(vorname, nachname);
+		
 		this.monatsLohn = monatsLohn;
+		
+		if(monatsLohn >= 8.84f * 160) {
+			System.out.println("Lohn unter Mindestlohn! ");
+		}
 	}
 
 	public float getMonatsLohn() {
@@ -19,5 +27,21 @@ public class ATAngestellter extends Mitarbeiter {
 	public float entgeltBerechnen() {
 		this.jahresGehaltBisHeute += monatsLohn - monatsLohn * 0.19325;
 		return monatsLohn;
+	}
+
+	
+	public float getJahresGehaltBisHeute() {
+		
+		return jahresGehaltBisHeute;
+	}
+
+	@Override
+	public float tatsaechlicheEinkommenSteuer() {
+		return this.jahresGehaltBisHeute*EINKOMMENSSTEUERSATZ;
+	}
+
+	@Override
+	public float voraussichtlicheEinkommenSteuer() {
+		return this.monatsLohn*12*EINKOMMENSSTEUERSATZ;
 	}
 }
